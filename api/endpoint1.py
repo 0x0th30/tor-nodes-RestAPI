6e7f1b71b7382ddbbbbb5b4638b2fff48b6a028d
theo <theodorotheodore@gmail.com>
2021-05-19 11:20:17 -0300

    O nosso endpoint ja e capaz de obter os ips da primeira fonte

---
+++ api/endpoint1.py
@@ -0,0 +1,29 @@
+# aqui eu realizei a importação das bibliotecas necessárias
+import requests, json
+
+# array que irá armazenar os nosso IP's
+ipList = list()
+
+# e nesta rotina é realizada a requisição GET para o primeiro endereço
+def firstTarget():
+    bruteJSON = requests.get("https://onionoo.torproject.org/summary?limit=5000")
+    newJSON = json.loads(bruteJSON.text)                     # conversão para JSON
+    relays = newJSON['relays']                               # acesso ao objeto "relays" do nosso JSON
+    
+    # iteração for que adiciona os IP's encontrados a um array pré-definido
+    for ip in relays:
+        ipList.append(ip['a'][0])
+
+
+# método que irá chamar as duas funções
+def getAll():
+    firstTarget()
+    
+    # laço for para verificação de possíveis erros
+    i = 1
+    for ip in ipList:
+        print(f'IP: {i}   ==>  {ip}')
+        i += 1
+
+
+getAll()
